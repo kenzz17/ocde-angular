fece94ce5e2bfbfc60d0b2ad3c46af13f088ceed
aniket-agrawal <aniket9636541817@gmail.com>
2020-12-05 19:40:11 +0530

    2

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -4,6 +4,8 @@ import { CompilerService } from '../compiler.service';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { Out } from '../Out';
 import { FILES } from '../mock-files';
+import { File } from '../File';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-editor',
@@ -13,6 +15,11 @@ import { FILES } from '../mock-files';
 export class EditorComponent implements OnInit {
 
   files = FILES;
+  file_selected = 'untitled';
+  new_file='';
+  is_add=false;
+
+  create = '';
 
   selected = 'cpp';
   stdin = '';
@@ -31,7 +38,7 @@ export class EditorComponent implements OnInit {
   
   // initial value of code --> should be different for different languages 
   code: string = '#include <iostream>\nusing namespace std;\n\nint main(){\n\t\n\treturn 0;\n}';
-  par_code: string = this.code;
+  new_code = '';
 
   constructor(private compileService: CompilerService, private modalService: NgbModal) { }
 
@@ -45,20 +52,20 @@ export class EditorComponent implements OnInit {
       .subscribe(data => this.show(data));
   }
 
-  open(content) {
+  /*open(content) {
     this.modalService.open(content, 
       {ariaLabelledBy: 'modal-basic-title'}).result.then((result)=>{ 
          this.closeResult = `Closed with: ${result}`;
-         if(result=='open') this.code=this.par_code;
-         else this.par_code=this.code;
+         if(result=='open' && this.is_select) this.code=this.par_code;
+         this.is_select = false;
        }, (reason) => { 
          this.closeResult =  
             `Dismissed ${this.getDismissReason(reason)}`; 
        });
-  }
+  }*/
 
-  private getDismissReason(reason: any): string { 
-    this.par_code=this.code;
+  /*private getDismissReason(reason: any): string { 
+    this.is_select = false;
     if (reason === ModalDismissReasons.ESC) { 
       return 'by pressing ESC'; 
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) { 
@@ -66,7 +73,7 @@ export class EditorComponent implements OnInit {
     } else { 
       return `with: ${reason}`; 
     } 
-  } 
+  }*/
 
   show(data: Out): void {
     this.out = data;
@@ -80,8 +87,45 @@ export class EditorComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  s(code: string): void{
-    this.par_code = code;
+  s(code: string, name: string): void{
+    this.code = code;
+    this.file_selected = name;
+    this.selected = name.split('.')[1]
   }
 
+  save(): void {
+    for (let index = 0; index < this.files.length; index++) {
+      if(this.files[index].name==this.file_selected){
+        this.files[index].code=this.code;
+        return;
+      }
+    }
+    this.create = 'save';
+    this.is_add = true;
+    this.new_code = this.code;
+  }
+  
+
+  del(name: string): void{
+    this.files = this.files.filter(function(file){
+      return file.name!=name;
+    });
+    if(name==this.file_selected){
+      this.file_selected='untitled';
+    }
+  }
+
+  add(): void{
+    this.create = 'add'
+    this.is_add = true;
+    this.new_code = '';
+  }
+
+  
+
+  fin_add(): void{
+    this.files.push({name: this.new_file, code: this.new_code});
+    this.new_file = '';
+    this.is_add = false;
+  }
 }
