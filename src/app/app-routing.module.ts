48002a799adcd8ac83364b7e2a1954a3ffb6ee66
aniket-agrawal <aniket9636541817@gmail.com>
2020-12-08 20:12:33 +0530

    pass change added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { EditorComponent } from './editor/editor.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegComponent } from './reg/reg.component';
+import { PasschangeComponent } from './passchange/passchange.component';
+
 const routes: Routes = [
   { path: 'editor', component: EditorComponent },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/reg', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'reg', component: RegComponent },
+  { path: 'passchange', component: PasschangeComponent },
 ];
 
 @NgModule({
---
+++ src/app/passchange/passchange.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasschangeComponent } from './passchange.component';
+
+describe('PasschangeComponent', () => {
+  let component: PasschangeComponent;
+  let fixture: ComponentFixture<PasschangeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PasschangeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PasschangeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
